ccf2585c589de25b8a643afbdeba1ff562a33a29
Huzaifa <Huzaifaghouri206@gmail.com>
2024-12-26 20:10:29 +0500

    first commit

---
+++ src/scripts/index.ts
@@ -0,0 +1,7 @@
+document.getElementById('about-me')!.addEventListener('blur', () => {
+    console.log('About Me updated');
+});
+
+document.getElementById('download-btn')!.addEventListener('click', () => {
+    alert('Function to download resume coming soon!');
+});
